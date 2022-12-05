bdd98fa7cc5fc205f85c08c12ae6681e31d12d5b
svitlana-p <sweetlana.pukish@gmail.com>
2022-12-05 13:28:34 +0200

    changed styles to scss

--- emotions-project/src/app/features/test/test.component.ts
+++ emotions-project/src/app/features/test/test.component.ts
@@ -6,7 +6,7 @@ import { ITest } from 'src/app/models/test';
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
-  styleUrls: ['./test.component.css']
+  styleUrls: ['./test.component.scss']
 })
 export class TestComponent implements OnInit {
 
--- emotions-project/src/app/shared/circle-item/circle-item.component.ts
+++ emotions-project/src/app/shared/circle-item/circle-item.component.ts
@@ -3,7 +3,7 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-circle-item',
   templateUrl: './circle-item.component.html',
-  styleUrls: ['./circle-item.component.css']
+  styleUrls: ['./circle-item.component.scss']
 })
 export class CircleItemComponent implements OnInit {
   @Input() imgSrc = '';
